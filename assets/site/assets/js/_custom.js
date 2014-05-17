1e456ad36611501d5c8cee0bbe3e27784165c331
Chris Klongpayabal <chrisk@klongdesigns.com>
2014-05-17 16:09:32 -0500

    Dropdown schedule functionality

--- assets/site/assets/js/_custom.js
+++ assets/site/assets/js/_custom.js
@@ -1,8 +1,8 @@
 /*jshint smarttabs:true */
 
-function updateNewsChevrons() {
-	$('.newsLinks li').each(function() {
-		var lheight = $(this).height(),
+function updateNewsChevrons(callout) {
+	$(callout).each(function() {
+		var lheight = $(this).outerHeight(),
 		chev = $(this).find('.fa'),
 		cheight = chev.height(),
 		position = (lheight/2) - (cheight/2);
@@ -10,9 +10,26 @@ function updateNewsChevrons() {
 	});
 }
 $(document).ready(function(){
-	updateNewsChevrons();
+	updateNewsChevrons('.newsLinks li');
+	updateNewsChevrons('.schedule li a');
 });
+$(document).on('click', '.schedule > li a', function(e) {
+	e.preventDefault();
+	if($(this).parent().hasClass('active')) {
+		$(this).parent().addClass('closing');
+		$(this).find('.fa').removeClass('fa-chevron-down').addClass('fa-chevron-right');
+	} else {
+		$(this).parent().addClass('active');
+		$(this).find('.fa').removeClass('fa-chevron-right').addClass('fa-chevron-down');
 
+	}
+	$(this).parent().children('ul').slideToggle(400,function() {
+		if($(this).parent().hasClass('closing')) {
+			$(this).parent().removeClass('active').removeClass('closing');
+		}
+	});
+});
 $(window).resize(function() {
-        updateNewsChevrons();
+        updateNewsChevrons('.newsLinks li');
+        updateNewsChevrons('.schedule li a');
     });
