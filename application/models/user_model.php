d4ed14b13fee1fa66bdf312fca6a7b31a5dee969
Jon Rhoads <jon@oodletech.com>
2014-05-29 17:44:33 -0400

    removed an invalid delete condition from users

--- application/models/user_model.php
+++ application/models/user_model.php
@@ -95,8 +95,7 @@ class User_model extends MY_Model
 			// If this ID Belongs to Other Tables - Dont Delete It
 			// @ return: Return a string of error for ajax
 			if( 
-				$this->count_by( 'division_id', $id, 'seasons' ) > 0
-				|| $this->count_by( 'captain_user_id', $id, 'teams' ) > 0
+				$this->count_by( 'captain_user_id', $id, 'teams' ) > 0
 				|| $this->count_by( 'player_id', $id, 'game_players_soccer' ) > 0
 				|| $this->count_by( 'player_id', $id, 'session_players' ) > 0
 				|| $this->count_by( 'official_id', $id, 'game_officials' ) > 0
