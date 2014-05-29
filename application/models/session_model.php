3efe04c82d5e85d530f37c2ce7f508a6721ddb87
Jon Rhoads <jon@oodletech.com>
2014-05-29 18:00:41 -0400

    Added the base files for sessions and did the table view

---
+++ application/models/session_model.php
@@ -0,0 +1,99 @@
+<?php
+class Session_model extends MY_Model
+{
+	// Callbacks to MY_Model class to Run Before Record Inserts
+	public $before_create = array( 'created_at', 'created_by' );
+	public $before_update = array( 'modified_by' );
+	public $return_type = 'array';
+	public $before_dropdown = array( 'order_by(name)' );
+
+	// Get Records
+	public function get_records( )
+	{
+		// Construct Query
+		$this->db->select( 's.id, s.name, s.created_at, s.modified_at, seasons.name as season_name' );
+		$this->db->join( 'seasons', 'seasons.id = s.season_id', 'left outer' );
+
+		// Run Query
+		$query = $this->db->get( 'sessions s' );
+
+		// If Rows Were Found, Return Them
+		if($query->num_rows > 0)
+		{
+			$rows = $query->result_array();
+			return $rows;
+		}
+
+		return false;
+	}
+
+	// Add Record
+	public function insert_record( $post = FALSE )
+	{
+		if( $post )
+		{
+			// Insert Data
+			$data = array(
+				'name' => $post['name'],
+				'year_start' => $post['year_start'],
+				'year_end' => $post['year_end'],
+				'description' => empty( $post['description'] ) ? NULL : $post['description']
+			);
+
+			// Insert to Database and Store Insert ID
+			$insert_id = $this->insert( $data );
+
+			return $insert_id;
+		}
+
+		return false;
+	}
+
+	// Edit Record
+	public function update_record( $id = FALSE, $post = FALSE )
+	{
+		if( $id && $post )
+		{
+			// Update Data
+			$data = array(
+				'name' => $post['name'],
+				'year_start' => $post['year_start'],
+				'year_end' => $post['year_end'],
+				'description' => empty( $post['description'] ) ? NULL : $post['description']
+			);
+
+			// Update Record in Database
+			$this->update( $id, $data );
+
+			return true;
+		}
+
+		return false;
+	}
+
+	// Delete record
+	public function delete_record( $id = FALSE )
+	{
+		// If an ID Was Found in URL
+		if( $id )
+		{
+			// If this ID Belongs to Other Tables - Dont Delete It
+			// @ return: Return a string of error for ajax
+			if( 
+				$this->count_by( 'season_id', $id, 'sessions' ) > 0 
+				|| $this->count_by( 'current_season_id', $id, 'leagues' ) > 0
+			)
+			{
+				echo 'error';
+			}
+			// Else Delete It from Database
+			else
+			{
+				$this->Season_model->delete( $id );
+			}
+		}
+
+		return false;
+	}
+
+}
