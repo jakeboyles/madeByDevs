d7b2127a32457cff8638d8e186baabda7896d38b
Jon Rhoads <jon@oodletech.com>
2014-06-02 16:27:09 -0400

    Worked on Add a Location, and added a state helper file

---
+++ application/controllers/admin/locations.php
@@ -0,0 +1,103 @@
+<?php
+class Locations extends Admin_Controller
+{
+	// Run Class Construct
+	function __construct()
+	{
+		// Inherit Parent Classes Methods and Properties
+		parent::__construct();
+
+		// Load Database Model to Be Used in Methods
+		$this->load->model( 'Location_model' );
+
+		// Load State Helepr
+		$this->load->helper('state');
+	}
+
+	// Display All Records View
+	public function index()
+	{
+		$data['records'] = $this->Location_model->get_records();
+		$this->load->admin_template( 'locations', $data );
+	}
+
+	// Add New Record View
+	public function add()
+	{
+		// If Form is Submitted Validate Form Data and Add Record to Database
+		if( $this->input->post() && $this->_validation() )
+		{
+			// If Successfully Inserted to DB, Redirect to Edit
+			if( $insert_id = $this->Location_model->insert_record( $this->input->post() ) )
+			{
+				redirect('admin/locations/edit/' . $insert_id);
+			}
+		}
+
+		// Load Add Record Form View
+		$this->load->admin_template( 'locations_add' );
+	}
+
+	// Edit Record View
+	public function edit( $id = FALSE )
+	{
+		// If Form is Submitted Validate Form Data and Updated Record in Database
+		if( $this->input->post() && $this->_validation() && $id )
+		{
+			$this->Location_model->update_record( $id, $this->input->post() );
+		}
+
+		// Load User Agent Library for Referrer Add Record Message
+		$this->load->library('user_agent');
+
+		// Retrieve Record Data From Database
+		$data['record'] = $this->Location_model->get( $id );
+
+		// Load Edit Record Form
+		$this->load->admin_template( 'locations_edit', $data );
+	}
+
+	// Delete a Record
+	public function delete( $id = FALSE )
+	{
+
+		if( $id )
+		{
+			$this->Location_model->delete_record( $id );
+
+			return true;
+		}
+
+		return false;
+	}
+
+	// Run Validation on Create / Edit Forms
+	private function _validation()
+	{
+		// Load Validation Library
+		$this->load->library('form_validation');
+		
+		// Validation Rules
+		$this->form_validation->set_rules('name', 'Location Name', 'required');
+		$this->form_validation->set_rules('phone', 'Phone', '');
+		$this->form_validation->set_rules('website', 'Website', '');
+		$this->form_validation->set_rules('street_address', 'Street Address', '');
+		$this->form_validation->set_rules('street_address_2', 'Street Address 2', '');
+		$this->form_validation->set_rules('city', 'City', '');
+		$this->form_validation->set_rules('state', 'State', '');
+		$this->form_validation->set_rules('postal', 'Postal', '');
+		$this->form_validation->set_rules('map_latitude', 'Map Latitude', '');
+		$this->form_validation->set_rules('map_longitude', 'Map Longitude', '');
+		$this->form_validation->set_rules('map_zoom', 'Map Zoom', '');
+		$this->form_validation->set_rules('description', 'Location Description', '');
+		
+		// Return True if Validation Passes
+		if ($this->form_validation->run())
+		{
+			return true;
+		}
+		
+		return false;
+	}
+
+}
---
+++ application/models/location_model.php
@@ -0,0 +1,107 @@
+<?php
+class Location_model extends MY_Model
+{
+	// Callbacks to MY_Model class to Run Before Record Inserts
+	public $before_create = array( 'created_at', 'created_by' );
+	public $before_update = array( 'modified_by' );
+	public $return_type = 'array';
+	public $before_dropdown = array( 'order_by(name)' );
+
+	// Get Records
+	public function get_records( )
+	{
+		// Construct Query
+		$this->db->select( 's.id, s.name, s.year_start, s.year_end, s.created_at, s.modified_at, l.name as league_name' );
+		$this->db->join( 'leagues l', 'l.id = s.league_id', 'left outer' );
+
+		// Run Query
+		$query = $this->db->get( 'seasons s' );
+
+		// If Rows Were Found, Return Them
+		if($query->num_rows > 0)
+		{
+			$rows = $query->result_array();
+			return $rows;
+		}
+
+		return false;
+	}
+
+	// Add Record
+	public function insert_record( $post = FALSE )
+	{
+		if( $post )
+		{
+			// Insert Data
+			$data = array(
+				'name' => $post['name'],
+				'phone' => empty( $post['phone'] ) ? NULL : $post['phone'],
+				'website' => empty( $post['website'] ) ? NULL : $post['website'],
+				'street_address' => empty( $post['street_address'] ) ? NULL : $post['street_address'],
+				'street_address_2' => empty( $post['street_address_2'] ) ? NULL : $post['street_address_2'],
+				'city' => empty( $post['city'] ) ? NULL : $post['city'],
+				'state' => empty( $post['state'] ) ? NULL : $post['state'],
+				'postal' => empty( $post['postal'] ) ? NULL : $post['postal'],
+				'map_latitude' => empty( $post['map_latitude'] ) ? NULL : $post['map_latitude'],
+				'map_longitude' => empty( $post['map_longitude'] ) ? NULL : $post['map_longitude'],
+				'map_zoom' => empty( $post['map_zoom'] ) ? NULL : $post['map_zoom'],
+				'description' => empty( $post['description'] ) ? NULL : $post['description']
+			);
+
+			// Insert to Database and Store Insert ID
+			$insert_id = $this->insert( $data );
+
+			return $insert_id;
+		}
+
+		return false;
+	}
+
+	// Edit Record
+	public function update_record( $id = FALSE, $post = FALSE )
+	{
+		if( $id && $post )
+		{
+			// Update Data
+			$data = array(
+				'name' => $post['name'],
+				'year_start' => $post['year_start'],
+				'year_end' => $post['year_end'],
+				'description' => empty( $post['description'] ) ? NULL : $post['description']
+			);
+
+			// Update Record in Database
+			$this->update( $id, $data );
+
+			return true;
+		}
+
+		return false;
+	}
+
+	// Delete record
+	public function delete_record( $id = FALSE )
+	{
+		// If an ID Was Found in URL
+		if( $id )
+		{
+			// If this ID Belongs to Other Tables - Dont Delete It
+			// @ return: Return a string of error for ajax
+			if( 
+				$this->count_by( 'season_id', $id, 'sessions' ) > 0 
+				|| $this->count_by( 'current_season_id', $id, 'leagues' ) > 0
+			)
+			{
+				echo 'error';
+			}
+			// Else Delete It from Database
+			else
+			{
+				$this->Season_model->delete( $id );
+			}
+		}
+
+		return false;
+	}
+
+}
