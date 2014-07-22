ddc8ea61cc638fdf79d285790a5a84b41bb7a3aa
Jon Rhoads <jon@oodletech.com>
2014-07-22 17:26:53 -0400

    Made a slight change to MY_Loader.php

--- application/core/MY_Loader.php
+++ application/core/MY_Loader.php
@@ -17,7 +17,7 @@ class MY_Loader extends CI_Loader {
 	public function admin_template($template_name, $vars = array(), $return = FALSE)
 	{
 		$content  = $this->view('admin/_header', $vars, $return);
-		$content  = $this->view('admin/_sidebar', $vars, $return);
+		$content .= $this->view('admin/_sidebar', $vars, $return);
 		$content .= $this->view('admin/' . $template_name, $vars, $return);
 		$content .= $this->view('admin/_footer', $vars, $return);
 
