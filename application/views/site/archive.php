131077de090f194008ef011ff239a99ab39d243e
Jon Rhoads <jon@oodletech.com>
2014-07-25 11:45:15 -0400

    Archive Page html structure

--- application/views/site/archive.php
+++ application/views/site/archive.php
@@ -2,4 +2,27 @@
 	<h1><?php echo $category['name']; ?></h1>
 	
 	<!-- Loop Through the Posts -->
+	<?php if( !empty( $posts ) ): ?>
+
+		<?php foreach( $posts as $post ): ?>
+
+			<div class="post-entry">
+				<h2 class="post-title">
+					<a href="<?php echo base_url( $post['slug'] ); ?>"><?php echo $post['title']; ?></a>
+				</h2>
+				<div class="post-date">
+					<?php echo date('F, d, Y', strtotime( $post['created_at'] ) ); ?>
+				</div>
+				<div class="post-content">
+					<?php echo $post['content']; ?>
+				</div>
+			</div>
+
+		<?php endforeach; ?>
+
+	<?php else: ?>
+
+		This archive does not currently have any posts.
+
+	<?php endif; ?>
 </div>
