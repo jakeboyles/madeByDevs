8e10d45c295a04c369e10c861a36cb071d8fdaa9
Jake Boyles <jakeboyles@Jon-Rhoadss-MacBook-Pro-2.local>
2014-09-08 10:20:05 -0400

    fixes

--- application/views/admin/teams_edit.php
+++ application/views/admin/teams_edit.php
@@ -22,37 +22,13 @@
 
 							<div class="col-md-8 col-sm-8 col-xs-8">
 
-								<!-- START Display Error Messages -->
-								<?php if( validation_errors() && $this->input->post() ): ?>
-								<div class="alert alert-error">
-									<h4>Form Submission Errors</h3>
-									<ul>
-									<?php echo validation_errors('<li>','</li>'); ?>
-									</ul>
-								</div>
-								<?php endif; ?>
-								<!-- END Display Error Messages -->
-
-								<!-- START Success Message -->
-								<?php if( !validation_errors() && $this->input->post() ): ?>
-								<div class="alert alert-success">
-									This record has been updated.
-								</div>
-								<?php endif; ?>
-								<!-- END Success Message -->
-								
-								<!-- START New Record Added Message -->
-								<?php if( $this->agent->is_referral() && $this->agent->referrer() == base_url('admin/teams/add') ): ?>
-								<div class="alert alert-success">
-									Record successfully added.
-								</div>
-								<?php endif; ?>
+			
 								<!-- END New Record Added Message -->
 
 								<!-- START Form -->
 								<?php echo form_open_multipart( 'admin/teams/edit/' . $record['id'], array( 'id' => 'edit-team-form') ); ?>
 
-									<!-- <div class="form-group">
+									<div class="form-group">
 										<?php echo form_label( 'Team Name*', 'name', array( 'class' => 'form-label' ) ); ?>
 										<?php echo form_input( array('name' => 'name', 'class' => 'form-control', 'id' => 'name', 'value' => set_value( 'name', $record['name'] ) ) ); ?>
 									</div>
@@ -79,7 +55,7 @@
 										<?php echo form_textarea( array('name' => 'additional_info', 'class' => 'form-control', 'id' => 'additional_information', 'value' => set_value( 'additional_information' , $record['additional_info'] ) ) ); ?>
 									</div>
 
-									<button type="submit" class="btn btn-primary">Edit Team</button> -->
+									<button type="submit" class="btn btn-primary">Edit Team</button>
 
 								<?php echo form_close(); ?>
 								<!-- END Form -->
