0028b232f592f44771b65de45eb7a1f2e2cdd630
Jake Boyles <jakeboyles@Jon-Rhoadss-MacBook-Pro-2.local>
2014-09-19 14:42:50 -0400

    fixes

--- application/views/site/home.php
+++ application/views/site/home.php
@@ -137,10 +137,12 @@
 			</div>
 
 			<?php else: ?>
-				<div class='col-md-12'>
-					<h2><a href="<?php echo base_url().$post['slug']; ?>"><?php echo $post['title']; ?></a></h2>
-					<?php echo $post['content']; ?>
-					<a class='btn btn-primary' href="<?php echo base_url().$post['slug']; ?>">Read More</a>
+				<div class="row">
+					<div class='col-md-12'>
+						<h2><a href="<?php echo base_url().$post['slug']; ?>"><?php echo $post['title']; ?></a></h2>
+						<?php echo $post['content']; ?>
+						<a class='btn btn-primary' href="<?php echo base_url().$post['slug']; ?>">Read More</a>
+					</div>
 				</div>
 			<?php endif; ?>
 		</div>
