d6173e53081ee64ac210a99ec51d6b6e66c763b8
Jake Boyles <jake@infotrustllc.com>
2015-01-07 23:12:53 -0500

    fixes

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -1,5 +1,5 @@
 <?php
-class Home extends CI_Controller
+class Home extends Site_Controller
 {
 	// Set Class Vars
 	var $user; // Set in correctLogin()
@@ -12,14 +12,14 @@ class Home extends CI_Controller
 	// Display Login Page or Log the User In
 	public function index()
 	{
-		// If the Login Form Was Submitted
-		if($this->input->post())
-		{
-			$this->_validation();
-		}
+		// // If the Login Form Was Submitted
+		// if($this->input->post())
+		// {
+		// 	$this->_validation();
+		// }
 
-		// Load Login Form View
-		//$this->load->view('admin/login');
+		// // Load Login Form View
+		// //$this->load->view('admin/login');
 		$data = array();
 		$this->load->site_template( 'home', $data );
 	}
