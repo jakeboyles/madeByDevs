a81f36f1a4d491471b1d99008fcc5a6eb8c96b02
Jake Boyles <jake@infotrustllc.com>
2015-02-10 21:03:09 -0500

    ga

--- application/views/site/_footer.php
+++ application/views/site/_footer.php
@@ -1,5 +1,14 @@
 <!-- Put footer stuff in here -- >
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
+  ga('create', 'UA-59619011-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
 		<!-- Footer JS Files -->
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 		<script src="<?php echo base_url('assets/site/assets/js/scripts.min.js'); ?>"></script>
