f994947d94de6070124b92afe3746d569ed7ae6f
Jake Boyles <jake@infotrustllc.com>
2015-02-10 21:17:44 -0500

    ga

--- application/views/admin/_footer.php
+++ application/views/admin/_footer.php
@@ -68,7 +68,7 @@
 <script src="<?php echo base_url(); ?>assets/admin/js/custom.js" type="text/javascript"></script>
 <!-- END CUSTOM JS -->
 
-<script>
+<script type="text/javascript">
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
